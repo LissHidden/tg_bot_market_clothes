cc7c67bbec6ec91d086f9c464769d720765fc323
LissHidden <lisshidden@icloud.com>
2024-10-23 18:32:20 +0300

    base

---
+++ tg_market.py
@@ -0,0 +1,12 @@
+from telebot import TeleBot
+
+TOKEN = '7651803254:AAFZJttR-BRLY_vzO9xkSShrGJXJcIOIerk'
+
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, 'Добро пожаловать в магазин! В ассортименте вы найдете стильную и комфортную одежду под нашим брендом.')
+
+bot.infinity_polling()
